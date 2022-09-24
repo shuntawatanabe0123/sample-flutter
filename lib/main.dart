607f2afe7d2cdac0ba10622c4a3c4b253c5c5987
shuntawatanabe0123 <resuta.0123@gmail.com>
2022-09-24 20:41:33 +0900

    変更しました

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        '変更しました',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
